8fa9867a7e6546bb3fdddffb6981f5fa3458b7bb
MamathaKP-create <mamatha.kp@subex.com>
2025-05-25 14:27:43 +0530

    first from X

---
+++ eclipse-workspace/GitDemoX/src/Gittesting1.java
@@ -0,0 +1,15 @@
+
+public class Gittesting1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		
+		
+	}
+
+}
